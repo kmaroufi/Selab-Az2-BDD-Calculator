569834b670222fcaa1f8c83dc3318d9f137d8121
vsaeide <vsaeide7798@gmail.com>
2021-10-26 19:23:34 +0330

    sqr function implemented

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,17 @@
 package calculator;
 
 public class Calculator {
+
     public Float reverse(int a) {
         if (a == 0) return null;
 
         return 1.0f / a;
+    }
+
+    public Float sqr(int a) {
+
+        if (a < 0) return null;
+
+        return (float) Math.sqrt(a);
     }
 }
